f20d897232e58d4ce6830d9a601ddfe10377b6c0
Robert <36878317+robbowz@users.noreply.github.com>
2018-08-11 16:44:16 +0200

    Update bxk_ubuntu_install.sh

--- bxk_ubuntu_install.sh
+++ bxk_ubuntu_install.sh
@@ -31,7 +31,7 @@ function install_packages() {
 	apt-get -y install libzmq3-dev  &>> ${SCRIPT_LOGFILE}
 	add-apt-repository -yu ppa:bitcoin/bitcoin  &>> ${SCRIPT_LOGFILE}
 	apt-get -y update &>> ${SCRIPT_LOGFILE}
-  	sudo apt-get install p7zip-full &>> ${SCRIPT_LOGFILE}
+  	apt-get install p7zip-full &>> ${SCRIPT_LOGFILE}
 	apt-get -y install wget make automake autoconf build-essential libtool autotools-dev \
 	git nano python-virtualenv pwgen virtualenv \
 	pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common \
